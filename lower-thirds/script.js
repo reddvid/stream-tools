030fd19085d65ba4ba2662a4c613661f7163a45f
David Ballesteros <david_ballesteros@outlook.com>
2024-02-13 19:20:44 +0800

    Before changing scroll

--- lower-thirds/script.js
+++ lower-thirds/script.js
@@ -8,3 +8,17 @@ setInterval(
   10000
 );
 updateTime();
+
+
+function changeTitleText(i) {
+ 
+  document.getElementById("maintext").innerHTML = i;
+}
+
+function changeSubText(i) {
+  document.getElementById("subtext").innerHTML = i;
+}
+
+function changeScrollText(i) {
+  document.getElementById("scrollingtext").innerHTML = i;
+}
